521787e344a42f1d94f319ce111bd3ffc57cb103
RAHUL KUMAR PANDEY <101944395+rkpandey5001@users.noreply.github.com>
2023-09-16 13:45:34 +0000

    datatype

---
+++ 01_Basics/datatype.js
@@ -0,0 +1,14 @@
+"use strict" //treat all JS code as newer version
+let username="Rahul";
+// console.log(username);
+
+// alert(3+3);// not allowed because we are using node js , and alert is run on browser
+
+//Datatypes
+let name="Rahul"; //String
+let age=18; //Number=> 2 to power 53
+let isLoggedIn=false; //Boolean
+let value; //undefined , value not initialized
+let number=null; // means empty and empty is not equal to undefined
+// console.log(typeof null); //type of null is object
+// console.log(typeof undefined); //type of null is undefined
--- 01_Basics/variables.js
+++ 01_Basics/variables.js
@@ -2,10 +2,11 @@ const accountId=14453;
 let accountEmail="Rahul";
 var accountPassword="12345";
 accountCity="Jaipur";
+let pincode;
 // accountId=2; not allowed because accountId is constant
 // accountEmail="Rahul Kumar Pandey";
 // console.log(accountEmail);
 {
     accountPassword="Rahul Kumar Pandey"; 
 }
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,pincode]);
